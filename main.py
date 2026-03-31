0c1b5667ca62b9f272034959a782ca4ffb52b268
jagernashrestha <jagerna1543@gmail.com>
2026-03-31 16:17:02 +0545

    Added iris dataset project files

---
+++ main.py
@@ -0,0 +1,8 @@
+def main():
+    print("Hello from 07-correlation-iris-dataset!")
+
+
+if __name__ == "__main__":
+    main()
+# >uv add seaborn // add seaborn and matplotlib
+# uv add ipykernel // for jupyter
